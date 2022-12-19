d97ce14a2c43f896ff74e17790d982e19ea109e7
vskvj3 <visakhvj3@gmail.com>
2022-12-20 00:04:21 +0530

    fixed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,40 @@
-import React from 'react'
-import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+import { React, useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(true);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
   return (
-    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-      <h1 className='w-full text-3xl font-bold text-[#00df9a] '>REACT.JS</h1>
-      <ul className='flex hidden'>
-        <li className='p-4'>Home</li>
-        <li className='p-4'>Company</li>
-        <li className='p-4'>Resources</li>
-        <li className='p-4'>About</li>
-        <li className='p-4'>Contact</li>
+    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
+      <h1 className="w-full text-3xl font-bold text-[#00df9a] ">REACT.JS</h1>
+      <ul className="hidden md:flex">
+        <li className="p-4">Home</li>
+        <li className="p-4">Company</li>
+        <li className="p-4">Resources</li>
+        <li className="p-4">About</li>
+        <li className="p-4">Contact</li>
       </ul>
-      <div>
-        <AiOutlineMenu size={20}/>
+      <div onClick={handleNav} className="block md:hidden">
+        {!nav? <AiOutlineMenu size={20}/> : <AiOutlineClose size={20}/>}
+
       </div>
-      <div>
-        <ul>
-        <li className='p-4'>Home</li>
-        <li className='p-4'>Company</li>
-        <li className='p-4'>Resources</li>
-        <li className='p-4'>About</li>
-        <li className='p-4'>Contact</li>
+      <div className= {nav? "fixed left-0 top-0 w-[60%] border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500 " : "fixed left-[-100%]"}>
+        <h1 className="mt-[30px] px-4 w-full text-3xl font-bold text-[#00df9a] ">
+          REACT.JS
+        </h1>
+        <ul className="p-4 pt-12 uppercase">
+          <li className=" border-b border-gray-600 p-4 ">Home</li>
+          <li className="border-b border-gray-600 p-4 ">Company</li>
+          <li className="border-b border-gray-600 p-4">Resources</li>
+          <li className="border-b border-gray-600 p-4">About</li>
+          <li className="p-4">Contact</li>
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
